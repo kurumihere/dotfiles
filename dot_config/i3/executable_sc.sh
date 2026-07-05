debbfd0e550f15e0bfb7c496e74c9a487bf49a5b
kurumihere <kurumihere@disroot.org>
2026-07-05 11:51:46 +0500

    Update `i3` config, to `dmenu` and delete display manager also delete service because i'ts not need yet

--- dot_config/i3/executable_sc.sh
+++ dot_config/i3/executable_sc.sh
@@ -7,32 +7,16 @@ mkdir -p "$dir"
 name="screenshot-$(date +%Y-%m-%d_%H-%M-%S).png"
 file="$dir/$name"
 
-mode="${1:-}"
-if [ -z "$mode" ]; then
-  mode="$(printf 'Area\nWindow\nFull\n' | rofi -dmenu -i -p screenshot)"
-  sleep 0.2
-fi
-
-case "$mode" in
-  Area|area)
-    maim --select --hidecursor --quiet "$file"
+case "${1:-area}" in
+  area|Area)
+    maim --select --hidecursor "$file"
     ;;
-  Window|window)
-    window_id="$(xprop -root _NET_ACTIVE_WINDOW | awk '{print $5}')"
-    case "$window_id" in
-      ""|0x0|0)
-        exit 1
-        ;;
-    esac
-    maim --window "$window_id" --hidecursor --quiet "$file"
-    ;;
-  Full|full)
-    maim --hidecursor --quiet "$file"
+  full|Full)
+    maim --hidecursor "$file"
     ;;
   *)
-    exit 0
+    exit 2
     ;;
 esac
 
 xclip -selection clipboard -t image/png -i "$file"
-notify-send -a screenshot -i "$file" "Screenshot" "$name copied"
